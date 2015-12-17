6e0af0d3c0ea8b6f4fa4fee73f009f87b582932a
Jonathan Sanderson <jonathan.sanderson@northumbria.ac.uk>
2015-12-17 10:57:32 +0000

    Comments clean-up and Readme completion.

--- IoTrees.py
+++ IoTrees.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 #-----------------------------------------------------------------------
 # Internet of Trees
-#  - sets Unicorn HAT colour from received Tweet data
-# Based on Twitter-Stream-Responder
-#
+# Run on a Raspberry Pi with Pimoroni Unicorn HAT; sets LED colour from 
+# received Tweet data.
+# 
+# Based heavilyon on example code from Python Twitter Tools, http://mike.verdone.ca/twitter/
 # Uses rgb_to_kelvin by petrklus, https://gist.github.com/petrklus/b1f427accdf7438606a6
 # itself based on work by Tanner Helland, http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
-#
 # Uses code based on Pimoroni Unicorn HAT examples,  https://github.com/pimoroni/unicorn-hat
+# Extract hex strings example from http://stackoverflow.com/questions/18737788/extract-hexadecimal-number-from-string
 #
-# Extract hex strings exmaple from http://stackoverflow.com/questions/18737788/extract-hexadecimal-number-from-string
-#
-# TODO: complete attributions here
+# Author: Jonathan Sanderson, for Think Physics, Northumbria University
+# 
 #-----------------------------------------------------------------------
 
 from kelvin_to_rgb import *
@@ -30,60 +30,45 @@ def colour_change(value):
             unicorn.show()
             time.sleep(0.05)
 
-#-----------------------------------------------------------------------
-# this is the username we're matching against.
-#-----------------------------------------------------------------------
+# Username we're treating tweets to as input
 username = "thinkphysicsne"
 
-#-----------------------------------------------------------------------
-# sleep for this number of seconds between tweets, to ensure we
-# don't flood
-#-----------------------------------------------------------------------
+# sleep between tweets to avoid flooding.
 sleep_time = 1
 
-#-----------------------------------------------------------------------
-# load our API credentials
-#-----------------------------------------------------------------------
+# load API credentials
 config = {}
 print("Loading config file\n")
 execfile("config.py", config)
 
-#-----------------------------------------------------------------------
-# create twitter API object
-#-----------------------------------------------------------------------
+# Create Twitter API object
 auth = OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"])
 twitter = Twitter(auth = auth)
 stream = TwitterStream(domain = "userstream.twitter.com", auth = auth, secure = True)
 
-#-----------------------------------------------------------------------
-# iterate over tweets matching this filter text
-#-----------------------------------------------------------------------
+# Iterate over tweets for this user stream
 tweet_iter = stream.user()
-
-
 for tweet in tweet_iter:
-    #-----------------------------------------------------------------------
-    # check whether this is a valid tweet
-    #-----------------------------------------------------------------------
+    # Does this tweet contain any parsed entities? If not, skip it.
     if "entities" not in tweet:
         continue
     
-    #-----------------------------------------------------------------------
-    # are we mentioned within this tweet?
-    #-----------------------------------------------------------------------
+    # Extract user mentions
     mentions = tweet["entities"]["user_mentions"]
     mentioned_users = [ mention["screen_name"] for mention in mentions ]
 	
+    # Are any of those mentions for the watched user?
     if username in mentioned_users:
+        # Output sender and tweet text
         print "\n ========================================= \n"
         print "Tweet from @%s" % tweet["user"]["screen_name"]
         print "Tweet text: %s" % tweet["text"]
         
-        # run hex extraction on text
+        # Run hex extraction on text
         extract_hex = re.findall(r'#[0-9A-Fa-f]{6}', tweet["text"], re.I)
         print "Extracted hex codes: %s" % extract_hex
         
-        # run Kelvin extraction on text
+        # Run Kelvin extraction on text
         extract_kelvin = re.findall(r'[0-9]{4,6}[kK]', tweet["text"], re.I)
         print "Extracted Kelvin temperatures: %s" % extract_kelvin
         
@@ -96,23 +81,18 @@ for tweet in tweet_iter:
             extract_kelvin = int(''.join(extract_kelvin)) # cast to integer
             kelvin_RGB = kelvin_to_rgb(extract_kelvin)
             print "Kelvin RGB target: %s" % (kelvin_RGB,)
-        
             print "Execute Unicorn to Kelvin RGB target"
             colour_change(kelvin_RGB)
         
-        # Test if we have any hex results, then:
+        # Kelvin input preferred (because Physics, obviously), but fall back to hex if that's present:
         elif len(extract_hex):
-            # discard all but first element, cast to string before passing to hex_to_rgb
+            # discard all but first match, cast to string before passing to hex_to_rgb
             hex_RGB = hex_to_rgb(''.join(extract_hex[0]))
             print "Hex RGB target: %s" % (hex_RGB,)
-        
             print "Execute Unicorn to Hex RGB target"
             colour_change(hex_RGB)
 	    
-        #-----------------------------------------------------------------------
-        # update our status with a thank you message directed at the source.
-        # use try/except to catch potential failures.
-        #-----------------------------------------------------------------------
+        # Remainder code from Twitter-Stream-Responder example, left as stuff for auto-thanks if one should wish to implement that
         # status = "@%s thanks for the mention" % tweet["user"]["screen_name"]
         # try:
         #     twitter.statuses.update(status = status)
@@ -120,4 +100,3 @@ for tweet in tweet_iter:
         #     print " - failed (maybe a duplicate?): %s" % e
         
     time.sleep(sleep_time)
-
--- readme.md
+++ readme.md
@@ -9,11 +9,42 @@ This code drives coloured lights from a Twitter stream. It's a ghastly hack, but
 To replicate Think Physics' IoTree, you'll need:
 
 1. A tree. We built [this design](http://www.instructables.com/id/Makedo-Christmas-tree/ "Makedo Cardboard Christmas tree: 2015 UPDATE") from Makedo on Instructables. Makedo is a terrific cardboard construction system with which you can quickly lash together big structures. We love it.
-2. A Raspberry Pi. We used a Pi2 backed onto a Pi touchscreen, in a Pimoroni stand. We'd planned to use a Pi Zero, but couldn't get our hands on one. The Pi needs to be on a network; we used a WiFi dongle, since plugging things into our university network is frowned upon.
-3. A Pimoroni Unicorn HAT, which is a nifty little 8x8 array of individually-addressable RGB LEDs.
+2. A [Raspberry Pi](https://www.raspberrypi.org). We used a Pi2 backed onto a [Pi touchscreen](https://www.raspberrypi.org/products/raspberry-pi-touch-display/), in a [Pimoroni stand](https://shop.pimoroni.com/products/raspberry-pi-7-touchscreen-display-with-frame), which all fitted neatly into the side of the Makedo Tree base. The Pi needs to be on a network; we used a [WiFi dongle](https://shop.pimoroni.com/products/official-raspberry-pi-wifi-dongle), since plugging things into our university network is frowned upon.
+3. A [Pimoroni Unicorn HAT](https://shop.pimoroni.com/products/unicorn-hat), which is a nifty 8x8 array of individually-addressable RGB LEDs.
 
 ## Software
 
-This code was copy-pasted together from example code and a bunch of StackExchange answers, then I spent a bunch of time trying to wrap my head around how to cast lists to strings and integers. There are more elegant ways of achieving the same results. At least, I hope there are, or all the good things I've heard about Python turn out to be complete fibs.
+Requires:
 
-TODO: complete this README.
+* Unicorn HAT Python bindings: `\curl -sS get.pimoroni.com/unicornhat | bash`, if you're trusting; see [here](https://github.com/pimoroni/unicorn-hat) if you'd like more information before piping a URL straight to shell.
+* [Python Twitter Tools](http://mike.verdone.ca/twitter/ "Python Twitter Tools (command-line client and IRC bot)"). Once installed, follow [these instructions](https://github.com/sixohsix/twitter/tree/master#working-with-oauth) to acquire the necessary Twitter OAuth keys.
+
+This code was copy-pasted together from examples and a bunch of StackExchange answers, with most of the "development" time spent with me trying to wrap my head around how to cast lists to string or integer. I've never written any Python before; pull requests which present better ways of doing things seen here are welcome!
+
+### Configuration
+
+Set `username` (line 34) to the Twitter handle you wish to follow. Tweets to this user will be processed.
+
+## Usage
+
+1. Tweet the target user with a hex colour code (as used in CSS): `@username #8a9a2a` will set the Unicorn HAT to the bright variant of Think Physics' theme green. The full 24-bit range `#000000` to `#FFFFFF` is available, though we can't speak to the colour accuracy of the LEDs. They're probably fairly terrible.
+2. Tweet the target user with a Kelvin colour temperature: `@username 3400K` will set the Unicorn HAT to a warm orange-white. The available range is 1000-15000K.
+
+At present, the code outputs diagnostics to STDOUT only. Stub code (from the Twitter Tools example) remains in place to tweet an acknowledgement in reply.
+
+### Kelvin temperature
+
+We're a physical sciences outreach project, so there had to be some physics in here somewhere. If only to justify the time investment. We'll write this up properly in a blog post, so for the moment:
+
+* Here's [Wikipedia's primer on colour temperature](https://en.wikipedia.org/wiki/Color_temperature) and black body radiation curves.
+* The Kelvin to RGB algorithm we're using is by [Tanner Helland, here](http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/ "How to Convert Temperature (K) to RGB: Algorithm and Sample Code &#8211; Tanner Helland (dot) com"), with the Python implementation referenced in the source itself.
+
+## Future development
+
+We plan two things:
+
+1. Integration with a [Pi Camera](https://www.raspberrypi.org/products/camera-module/) module to take tree selfies and post them in response to input tweets.
+
+2. Modification to replace the tree with a diffusing orb, and instrumentation so users can select a star temperature and have the orb glow the appropriate colour. Though colour conversion and perception are thorny subjects: see "[What colour is the Sun?](http://www.vendian.org/mncharity/dir3/starcolor/sun.html "What color is the Sun? - chromaticity above the atmosphere")" for an example.
+
+We're quite likely to hack something together for Maker Faire UK in April 2016. Look for us there!
